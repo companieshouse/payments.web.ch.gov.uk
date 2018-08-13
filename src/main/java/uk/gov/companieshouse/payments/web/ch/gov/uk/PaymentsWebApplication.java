082fb036742116787fb3c2e10ea6ee40eb925a6b
jphillips2121 <jphillips3@companieshouse.gov.uk>
2018-08-13 14:32:38 +0100

    Added barebones skeleton structure, pom.xml, and .gitignore file

---
+++ src/main/java/uk/gov/companieshouse/payments/web/ch/gov/uk/PaymentsWebApplication.java
@@ -0,0 +1,12 @@
+package uk.gov.companieshouse.payments.web.ch.gov.uk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsWebApplication.class, args);
+	}
+}
