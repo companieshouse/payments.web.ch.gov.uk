986b9bbaf087f6136c3c8eeb8584167ce25c5a99
jphillips2121 <jphillips3@companieshouse.gov.uk>
2018-08-14 12:21:18 +0100

    Changed tabs to spaces

--- src/main/java/uk/gov/companieshouse/payments/web/ch/gov/uk/PaymentsWebApplication.java
+++ src/main/java/uk/gov/companieshouse/payments/web/ch/gov/uk/PaymentsWebApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PaymentsWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaymentsWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentsWebApplication.class, args);
+    }
 }
