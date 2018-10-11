92d6e9badc0e44de5c216bd88be87b5aef67d9d7
Joseph-Nash <jnash@companieshouse.gov.uk>
2018-10-11 16:33:06 +0100

    change endpoint name to pay and associated test

--- src/main/java/uk/gov/companieshouse/web/payments/controller/PaymentSummaryController.java
+++ src/main/java/uk/gov/companieshouse/web/payments/controller/PaymentSummaryController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/payments/{paymentId}/payment-summary")
+@RequestMapping("/payments/{paymentId}/pay")
 public class PaymentSummaryController  {
 
     static final String PAYMENT_SUMMARY_VIEW = "payments/paymentSummary";
