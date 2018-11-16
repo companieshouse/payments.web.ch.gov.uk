4962d97462b2803f74796e99a7b5307ce454b35c
Joseph-Nash <jnash@companieshouse.gov.uk>
2018-11-16 09:10:15 +0000

    create test for payment interceptor

---
+++ src/test/java/uk/gov/companieshouse/web/payments/interceptor/PaymentInterceptorTest.java
@@ -0,0 +1,12 @@
+package uk.gov.companieshouse.web.payments.interceptor;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentInterceptorTest {
+
+    @Test
+    void preHandle() {
+    }
+}
