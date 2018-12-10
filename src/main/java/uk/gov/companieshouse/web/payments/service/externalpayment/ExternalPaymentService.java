50302b8e03d3a3ee87660a45bf6be394b86c9553
Jack Phillips <jphillips3@companieshouse.gov.uk>
2018-12-10 11:44:38 +0000

    Added JavaDoc comment to ExternalPaymentService

--- src/main/java/uk/gov/companieshouse/web/payments/service/externalpayment/ExternalPaymentService.java
+++ src/main/java/uk/gov/companieshouse/web/payments/service/externalpayment/ExternalPaymentService.java
@@ -2,6 +2,9 @@ package uk.gov.companieshouse.web.payments.service.externalpayment;
 
 import uk.gov.companieshouse.web.payments.exception.ServiceException;
 
+/**
+ * Interfaces with external payment providers. e.g: GovPay.
+ */
 public interface ExternalPaymentService {
 
     String createExternalPayment(String paymentId)
